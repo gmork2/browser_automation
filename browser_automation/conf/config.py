b6397de8b1eee126d47ad883599a5635b6acba2c
gmork2 <gmork.02@gmail.com>
2017-02-14 19:42:45 +0100

    Overrided several magic methods in conf.config.Config to mimic the ChainMap's behavior

--- browser_automation/conf/config.py
+++ browser_automation/conf/config.py
@@ -36,3 +36,72 @@ class Config(object):
         # Load the config from file in the config_dir
         self.__config_file = os.path.join(config_dir, 'conf', filename) \
                 if config_dir else get_default_config_dir(filename)
+
+    def __contains__(self, item):
+        return item in self.__config
+
+    def __bool__(self):
+        return bool(self.__config)
+
+    def __setitem__(self, key, value):
+        """
+        Sets item 'key' to 'value' in the config dictionary.
+        Does not allow changing the item's type unless it is None.
+        If the types do not match it will raise a ValueError.
+        """
+        # Do not allow the value type to change unless it is None
+        if key in self.__config \
+            and value is not None \
+            and not isinstance(self.__config[key], type(None)) \
+            and not isinstance(self.__config[key], type(value)):
+            logger.error('Value Type "{0}" invalid for key: {1}|{2}'
+                           .format(type(value), key, type(self.__config[key])))
+            raise ValueError
+        # Convert any key object to lower string
+        else:
+            self.__config[str(key).lower()] = value
+            logger.debug('Setting key "{0}" to: {1} (of type: {2})'
+                         .format(key, value, type(value)))
+
+    def __getitem__(self, key):
+        """
+        Get item with a specific key from the configuration. Lookups search the
+        underlying mappings successively until a key is found.
+        """
+        try:
+            return self.__config[key]
+        except KeyError:
+            pass
+        return self.__missing__(key)
+
+    def __missing__(self, key):
+        raise KeyError(key)
+
+    def __delitem__(self, key):
+        """
+        Delete item with a specific key from the configuration. Deletions only
+        operate on the first mapping.
+        """
+        del self.__config[key]
+
+    def __del__(self):
+        pass
+
+    def get(self, key, default):
+        return self[key] if key in self else default
+
+    def new_child(self, m=None):
+        return self.__config.new_child(m)
+
+    @property
+    def parents(self):
+        return self.__config.parents
+
+    def popitem(self):
+        return self.__config.popitem()
+
+    def pop(self, key, args):
+        return self.__config.pop(key, args)
+
+    def clear(self):
+        self.__config.clear()
