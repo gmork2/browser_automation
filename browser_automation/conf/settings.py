af039bfaf322505142c1b4e77eef8680d2df5a7c
gmork2 <gmork.02@gmail.com>
2017-02-15 17:06:00 +0100

    Modified settings to include logging configuration

--- browser_automation/conf/settings.py
+++ browser_automation/conf/settings.py
@@ -4,3 +4,46 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# The callable to use to configure logging
+LOGGING_CONFIG = 'logging.config.dictConfig'
+
+# Default logging. If you don’t want to configure logging at all
+# (or you want to manually configure logging using your own approach),
+# set LOGGING_CONFIG to None and provide a new configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'browser_automation.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'browser_automation.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'debug.log'),
+        },
+    },
+    'loggers': {
+        'browser_automation': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'INFO',
+        },
+    }
+}
