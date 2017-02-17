7f34011855b33109ea41ea83d510012f074e9360
gmork2 <gmork.02@gmail.com>
2017-02-17 10:54:23 +0100

    Updated settings to support mail infrastructure

--- browser_automation/conf/settings.py
+++ browser_automation/conf/settings.py
@@ -5,11 +5,37 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Default content type and charset to use for all email messages, if a
+# MIME type isn't manually specified. These are used to construct the
+# Content-Type header.
+DEFAULT_CONTENT_TYPE = 'text/html'
+DEFAULT_CHARSET = 'utf-8'
+
+# Encoding of files read from disk.
+FILE_CHARSET = 'utf-8'
+
+# Email address that error messages come from.
+SERVER_EMAIL = 'root@localhost'
+
 # The email backend to use. The default is to use the SMTP backend.
 # Third-party backends can be specified by providing a Python path
 # to a module that defines an EmailBackend class.
 EMAIL_BACKEND = 'browser_automation.core.mail.smtp.EmailBackend'
 
+# Host for sending email.
+EMAIL_HOST = 'localhost'
+
+# Port for sending email.
+EMAIL_PORT = 25
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_SSL_CERTFILE = None
+EMAIL_SSL_KEYFILE = None
+EMAIL_TIMEOUT = None
 # The callable to use to configure logging
 LOGGING_CONFIG = 'logging.config.dictConfig'
 
