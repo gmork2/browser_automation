dfccd777ce10d1938dc4b29959fab8dbe636f46f
gmork2 <gmork.02@gmail.com>
2017-02-17 11:40:56 +0100

    Added force_bytes function to utils

--- browser_automation/utils/encoding.py
+++ browser_automation/utils/encoding.py
@@ -8,9 +8,22 @@ from decimal import Decimal
 
 logger = logging.getLogger(__name__)
 
+WHITESPACE = re.compile('\s+')
+
 _PROTECTED_TYPES = (
     int, type(None), float, Decimal, datetime.datetime, datetime.date, datetime.time
 )
+
+class Promise(object):
+    """
+    This is just a base class for the proxy class created in
+    the closure of the lazy function. It can be used to recognize
+    promises in code.
+    """
+    pass
+
+def normalize_whitespace(string):
+    return WHITESPACE.sub(' ', string)
 
 def is_protected_type(obj):
     """
@@ -52,3 +65,37 @@ def force_text(s, encoding='utf-8', strings_only=False, errors='strict'):
             s = ' '.join(force_text(arg, encoding, strings_only, errors)
                          for arg in s)
     return s
+
+def force_bytes(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Similar to force_text, except that lazy instances are resolved to
+    bytestrings.
+    """
+    # Handle the common case first for performance reasons.
+    if isinstance(s, bytes):
+        if encoding == 'utf-8':
+            return s
+        else:
+            return s.decode('utf-8', errors).encode(encoding, errors)
+    if strings_only and is_protected_type(s):
+        return s
+    if isinstance(s, memoryview):
+        return bytes(s)
+    if isinstance(s, Promise):
+        return str(s).encode(encoding, errors)
+    if not isinstance(s, str):
+        try:
+
+            return str(s).encode(encoding)
+
+        except UnicodeEncodeError:
+            if isinstance(s, Exception):
+                # An Exception subclass containing non-ASCII data that doesn't
+                # know how to print itself properly. We shouldn't raise a
+                # further exception.
+                return b' '.join(force_bytes(arg, encoding, strings_only, errors)
+                                 for arg in s)
+            return str(s).encode(encoding, errors)
+    else:
+        return s.encode(encoding, errors)
+
