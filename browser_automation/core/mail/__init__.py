a86fcd40cf38a144c0ef8c35dbbb4c43b40d54d1
gmork2 <gmork.02@gmail.com>
2017-02-26 15:51:27 +0100

    Added send_mail function to core.mail

--- browser_automation/core/mail/__init__.py
+++ browser_automation/core/mail/__init__.py
@@ -1,5 +1,6 @@
-from utils.loading import import_string
 from conf import config
+from core.mail.message import EmailMultiAlternatives, EmailMessage
+from utils.loading import import_string
 
 
 def get_connection(backend=None, fail_silently=False, **kwds):
@@ -13,3 +14,26 @@ def get_connection(backend=None, fail_silently=False, **kwds):
     """
     cls = import_string(backend or config['email_backend'])
     return cls(fail_silently=fail_silently, **kwds)
+
+def send_mail(subject, message, from_email, recipient_list,
+              fail_silently=False, auth_user=None, auth_password=None,
+              connection=None, html_message=None):
+    """
+    Easy wrapper for sending a single message to a recipient list. All members
+    of the recipient list will see the other recipients in the 'To' field.
+
+    If auth_user is None, the EMAIL_HOST_USER setting is used.
+    If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
+    """
+    cls = import_string(config['email_backend'])
+    connection = connection or cls(
+        username=auth_user,
+        password=auth_password,
+        fail_silently=fail_silently,
+    )
+    mail = EmailMultiAlternatives(subject, message, from_email, recipient_list, connection=connection)
+    if html_message:
+        mail.attach_alternative(html_message, 'text/html')
+
+    return mail.send()
+
