e88c60441f7c5c739bdb7dcbdef0eca6a4604f8d
gmork2 <gmork.02@gmail.com>
2017-02-26 15:29:52 +0100

    New CommandParser class based in argparse.ArgumentParser

---
+++ browser_automation/core/commands/parser.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+
+from core.commands.exceptions import CommandError
+
+
+class CommandParser(ArgumentParser):
+    """
+    Customized ArgumentParser class to improve some error messages and prevent
+    SystemExit in several occasions, as SystemExit is unacceptable when a
+    command is called programmatically.
+    """
+    def __init__(self, cmd, **kwargs):
+        self.cmd = cmd
+        super(CommandParser, self).__init__(**kwargs)
+
+    def parse_args(self, args=None, namespace=None):
+        # Catch missing argument for a better error message
+        if (hasattr(self.cmd, 'missing_args_message') and
+                not (args or any(not arg.startswith('-') for arg in args))):
+            self.error(self.cmd.missing_args_message)
+        return super(CommandParser, self).parse_args(args, namespace)
+
+    def error(self, message):
+        if self.cmd._called_from_command_line:
+            super(CommandParser, self).error(message)
+        else:
+            raise CommandError("Error: %s" % message)
+
