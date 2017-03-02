8c0b8d34dfbe115d6f9d844084f23df0dc3ac3bd
gmork2 <gmork.02@gmail.com>
2017-03-02 18:40:42 +0100

    Implemented email.Aplication for check email sending functionality

--- browser_automation/core/commands/email.py
+++ browser_automation/core/commands/email.py
@@ -1,12 +1,25 @@
+import socket
+
+from core.mail import send_mail
 from core.commands.base import Command
 
 
 class Aplication(Command):
     help = "Sends a test email to the email addresses specified as arguments."
     missing_args_message = "You must specify some email recipients, or pass the --managers or --admin options."
-    
+
     def add_arguments(self, parser):
-        pass
-    
+        parser.add_argument(
+            'email', nargs='*',
+            help='One or more email addresses to send a test email to.',
+        )
+
     def handle(self, *args, **kwargs):
-        pass
+        subject = 'Test email from %s' % (socket.gethostname())
+
+        send_mail(
+            subject=subject,
+            message="If you\'re reading this, it was successful.",
+            from_email=None,
+            recipient_list=kwargs['email'],
+        )
