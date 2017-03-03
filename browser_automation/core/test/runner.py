5e19a91f5a4fda575a1ffc0f822f07a9c557df5a
gmork2 <gmork.02@gmail.com>
2017-03-03 21:53:47 +0100

    New TestProgram class based in unittest.TestProgram

--- browser_automation/core/test/runner.py
+++ browser_automation/core/test/runner.py
@@ -14,4 +14,21 @@ class HTMLTestRunner(object):
         self.stream = stream
 
 
+class TestProgram(unittest.TestProgram):
+    """
+    A variation of the unittest.TestProgram. Please refer to the base
+    class for command line parameters. In the future we may
+    build our own launcher to support more specific command line
+    parameters.
+    """
+    def runTests(self):
+        # Pick HTMLTestRunner as the default test runner.
+        # base class's testRunner parameter is not useful because it means
+        # we have to instantiate HTMLTestRunner before we know self.verbosity.
+        if self.testRunner is None:
+            self.testRunner = HTMLTestRunner(verbosity=self.verbosity)
+        super().runTests()
+
+# Facilities for running tasks from the command line
+main = TestProgram
 
