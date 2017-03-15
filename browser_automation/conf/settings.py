8f725ad55d40c9f7ecd8236e28af97886cb36cc9
gmork2 <gmork.02@gmail.com>
2017-03-15 22:14:38 +0100

    Added TEMPLATE_DIR setting to conf

--- browser_automation/conf/settings.py
+++ browser_automation/conf/settings.py
@@ -49,6 +49,9 @@ EMAIL_SSL_CERTFILE = None
 EMAIL_SSL_KEYFILE = None
 EMAIL_TIMEOUT = None
 
+# Templates folders
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 # Use {HOST}, {PORT} or {FILE}
 SERVICE_OPTIONS = "--port={PORT}"
 SERVICE_EXECUTABLE = os.path.join(BASE_DIR, 'bin', 'drivers', 'chromedriver.exe')
