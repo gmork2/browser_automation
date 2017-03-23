6dd3a98e2c58130eb8c0c08c75044c8cb9d9ff8b
gmork2 <gmork.02@gmail.com>
2017-03-23 22:39:07 +0100

    New USE_JINJA setting to use this template engine in HTML report generation

--- browser_automation/conf/settings.py
+++ browser_automation/conf/settings.py
@@ -52,6 +52,8 @@ EMAIL_TIMEOUT = None
 # Templates folders
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+USE_JINJA = False
+
 # Use {HOST}, {PORT} or {FILE}
 SERVICE_OPTIONS = "--port={PORT}"
 SERVICE_EXECUTABLE = os.path.join(BASE_DIR, 'bin', 'drivers', 'chromedriver.exe')
