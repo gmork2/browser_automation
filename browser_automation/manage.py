b836eb6efbef3c756f636c7b0bd1b745ab023d21
gmork2 <gmork.02@gmail.com>
2017-03-28 21:14:44 +0200

    Launch app provided by APP setting

--- browser_automation/manage.py
+++ browser_automation/manage.py
@@ -2,8 +2,15 @@
 import logging
 import sys
 
+from conf import settings
+from core.commands.base import Command
+from utils.loading import import_string
+
 
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    app = import_string(settings.APP)
+    if issubclass(app, Command):
+        app().run_from_argv(sys.argv)
     sys.exit(0)
